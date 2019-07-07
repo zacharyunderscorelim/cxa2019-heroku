5fce05dd3629d0dc7d09596fb89bffba57679b37
dbsqwerty123 <dbsqwerty123@gmail.com>
2019-07-07 23:01:18 +0800

    -
    -

--- herokuapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from herokuapp.models import Category, Page, UserProfile
-admin.site.register(Category)
-admin.site.register(Page)
-admin.site.register(UserProfile)
