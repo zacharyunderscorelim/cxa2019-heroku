dc045f613b1b32904cc986cdf9b711c3f44f7e75
zacharyunderscorelim <52022882+zacharyunderscorelim@users.noreply.github.com>
2019-07-08 12:05:19 +0800

    Update models.py

--- herokuapp/models.py
+++ herokuapp/models.py
@@ -6,13 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     def __str__(self):
         return self.name
-class Page(models.Model):
-    category = models.ForeignKey(Category, on_delete = models.PROTECT)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-    def __str__(self):
-        return self.title
 
 class UserProfile(models.Model):
     # This line is required. Links UserProﬁle to a User model instance.
@@ -21,4 +14,11 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='proﬁle_images', blank=True)
     def __str__(self):
         return self.user.username
-
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.PROTECT)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+ 
