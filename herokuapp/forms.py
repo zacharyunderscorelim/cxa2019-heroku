ba2ee594a1a2095ac5e12ec121163b5faecbf98e
zacharyunderscorelim <52022882+zacharyunderscorelim@users.noreply.github.com>
2019-07-08 15:27:23 +0800

    Update forms.py

--- herokuapp/forms.py
+++ herokuapp/forms.py
@@ -15,25 +15,15 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the specific food name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
-        if url and not url.startswith('http://'):
-            url = 'http://' + url
-            cleaned_data['url'] = url
-        return cleaned_data
     class Meta:
         # Provide an association between the ModelForm and a model
         model = Page
         # What ﬁelds do we want to include in our form?
         # This way we don't need every ﬁeld in the model present.
         # Some ﬁelds may allow NULL values, so we may not want to include them… # Here, we are hiding the foreign key.
-        fields = ('title', 'url', 'views')
+        fields = ('title', 'foodtype', 'views')
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(help_text="Please enter a username.")
